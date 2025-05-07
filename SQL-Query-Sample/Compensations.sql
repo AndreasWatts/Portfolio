27559b10fcc05ed933779c1f9c3ecd3a6b5b44cd
Andreas Watts <andreas.watts@gmail.com>
2025-05-07 11:53:00 +0200

    Create Compensations.sql

---
+++ SQL-Query-Sample/Compensations.sql
@@ -0,0 +1,21 @@
+--This script finds all compensations given to customers.
+
+SELECT 
+      SK_Date_Charge
+      ,U.SK_TransactionType
+	  ,TransactionText
+      ,[SK_SimCard]
+	  ,AccountType
+      ,[CostWithoutVat]
+      ,[VAT]
+  FROM [dbo].[fact].[vUsage] U
+LEFT JOIN 
+dim.vTransactionType ON U.SK_TransactionType = dim.vTransactionType.SK_TransactionType
+LEFT JOIN
+dim.vAccountType ON U.SK_AccountType = dim.vAccountType.SK_AccountType
+
+WHERE 
+	SK_Date_Charge >= 20220101
+	AND TransactionText LIKE '%Kompensation%'
+
+ORDER BY SK_Date_Charge ASC
