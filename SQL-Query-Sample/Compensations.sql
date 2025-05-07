9ea87e03b3187d76c6222b00160ecd937ae91b4e
Andreas Watts <andreas.watts@gmail.com>
2025-05-07 11:56:19 +0200

    Update Compensations.sql

--- SQL-Query-Sample/Compensations.sql
+++ SQL-Query-Sample/Compensations.sql
@@ -1,21 +1,67 @@
 --This script finds all compensations given to customers.
 
+-- Join ServiceID, product, Posting Type and activation+cancellation dato on table --
+
+DROP TABLE IF EXISTS #Compensationlist;
+
 SELECT 
-      SK_Date_Charge
-      ,U.SK_TransactionType
-	  ,TransactionText
-      ,[SK_SimCard]
-	  ,AccountType
-      ,[CostWithoutVat]
-      ,[VAT]
-  FROM [dbo].[fact].[vUsage] U
-LEFT JOIN 
-dim.vTransactionType ON U.SK_TransactionType = dim.vTransactionType.SK_TransactionType
+      Comp.SK_SimCard
+	  ,ServiceID
+	  ,SK_Date_Compensation
+      ,ProductLevel1
+	  ,ProductLevel2
+	  ,ProductLevel3
+	  ,ProductDisplayName
+      ,AgentComment
+      ,PostingType
+      ,Amount
+INTO #Compensationlist
+FROM fct.vCompensation Comp
 LEFT JOIN
-dim.vAccountType ON U.SK_AccountType = dim.vAccountType.SK_AccountType
+dim.vSimCard sc
+	ON Comp.SK_SimCard = sc.SK_SimCard
+LEFT JOIN
+dim.vBundle b
+	ON Comp.SK_Product = b.SK_Product
+LEFT JOIN
+dim.vAgentComment AGC
+	ON Comp.SK_AgentComment = AGC.SK_AgentComment
+LEFT JOIN
+dim.vPostingType PT
+	ON Comp.SK_PostingType = PT.SK_PostingType
 
-WHERE 
-	SK_Date_Charge >= 20220101
-	AND TransactionText LIKE '%Kompensation%'
+WHERE Comp.SK_Simcard <> -1
 
-ORDER BY SK_Date_Charge ASC
+-- Creating list with service ID og Activation Date + Cancellation Date --
+
+DROP TABLE IF EXISTS #ServiceIDList;
+
+SELECT DISTINCT
+	sub.SK_SimCard
+	,ServiceID
+	,SK_Date_Activation
+	,SK_Date_Cancellation
+INTO #ServiceIDList
+FROM fct.vSubscriber Sub
+LEFT JOIN 
+dim.vSimCard sc
+	ON Sub.SK_SimCard = sc.SK_SimCard
+
+-- Join Activation Date and Cancellation Date on Compenstions list --
+
+SELECT 
+	#Compensationlist.SK_SimCard
+	,#Compensationlist.ServiceID
+	,SK_Date_Compensation
+	,SK_Date_Activation
+	,SK_Date_Cancellation
+    ,BundleGroupLevel1
+	,BundleGroupLevel2
+	,BundleGroupLevel3
+	,BundleDisplayName
+    ,AgentComment
+    ,PostingType
+    ,Amount
+FROM #Compensationlist
+LEFT JOIN
+#ServiceIDList ON #Compensationlist.SK_SimCard = #ServiceIDList.SK_SimCard
